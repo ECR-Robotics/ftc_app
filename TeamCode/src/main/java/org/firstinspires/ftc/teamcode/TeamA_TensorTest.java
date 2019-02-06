e14ea1b560b6590bc3c3034e4e371f6273a6ede2
Cassidy <clangenfeld23@gmail.com>
2019-02-06 16:49:01 -0500

    untested tensorflow changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamA_TensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamA_TensorTest.java
@@ -128,39 +128,50 @@ public class TeamA_TensorTest extends LinearOpMode {
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
                         telemetry.addData("# Object Detected", updatedRecognitions.size());
-                        if (updatedRecognitions.size() == 3) {
-                            int goldMineralX = -1;
-                            int silverMineral1X = -1;
-                            int silverMineral2X = -1;
+                        if (updatedRecognitions.size() == 1) {
                             for (Recognition recognition : updatedRecognitions) {
                                 if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
-                                    goldMineralX = (int) recognition.getLeft();
-                                } else if (silverMineral1X == -1) {
-                                    silverMineral1X = (int) recognition.getLeft();
+                                    runtime.reset();
+                                    while (runtime.seconds() < 0.5) {
+                                        robot.move_straight(0.5);
+                                    }
+
                                 } else {
-                                    silverMineral2X = (int) recognition.getLeft();
-                                }
-                            }
-                            if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
-                                if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
-                                    telemetry.addData("Gold Mineral Position", "Left");
-                                } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
-                                    telemetry.addData("Gold Mineral Position", "Right");
-                                } else {
-                                    telemetry.addData("Gold Mineral Position", "Center");
+                                    runtime.reset();
+                                    while (runtime.seconds() < 0.3) {
+                                        robot.strafeLeft(0.3);
+                                    }
+                                    if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                        runtime.reset();
+                                        while (runtime.seconds() < 0.5) {
+                                            robot.move_straight(0.5);
+                                        }
+
+
+                                    } else {
+                                        runtime.reset();
+                                        while (runtime.seconds() < 0.6) {
+                                            robot.strafeRight(0.3);
+                                        }
+                                        while (runtime.seconds() < 0.5) {
+                                            robot.move_straight(0.5);
+                                        }
+
+                                        telemetry.update();
+                                    }
                                 }
                             }
                         }
-                        telemetry.update();
+
+                        if (tfod != null) {
+                            tfod.shutdown();
+                        }
                     }
                 }
             }
         }
-
-        if (tfod != null) {
-            tfod.shutdown();
-        }
     }
+
 
     /**
      * Initialize the Vuforia localization engine.
